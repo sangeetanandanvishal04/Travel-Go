adfca7defd229e9b0f4feafbf5048e864c9e82e6
Vishal Singh <sangeetanandanvishalsingh04@gmail.com>
2025-02-16 13:12:19 +0530

    Login&Signup route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException, Depends
+from .database import get_db, engine
+from sqlalchemy.orm import Session
+from . import schemas, tablesmodel, utils, oAuth2
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -14,6 +17,58 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+tablesmodel.Base.metadata.create_all(bind = engine)
+
 @app.get("/")
 def root():
     return {"message" : "Welcome to my API...."}
+
+""" {
+    "full_name": "Vishal Singh",
+    "email": "email@iiita.ac.in",
+    "password": "password",
+    "phone_number": "9876543210",
+    "country": "India",
+    "state": "Uttar Pradesh"
+} """
+@app.post("/signup", response_model=schemas.UserOut)
+async def create_user(user: schemas.UserCreate,  db: Session = Depends(get_db)):
+    user_found = db.query(tablesmodel.User).filter(tablesmodel.User.email == user.email).first()
+
+    if user_found:
+        raise HTTPException(status_code=status.HTTP_302_FOUND, detail="Email already exists")
+
+    hashed_password = utils.hash(user.password)
+    
+    new_user = tablesmodel.User(
+        full_name=user.full_name,
+        email=user.email,
+        password=hashed_password,
+        phone_number=user.phone_number,
+        country=user.country,
+        state=user.state
+    )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+""" {
+    "email": "email@iiita.ac.in",
+    "password": "password"
+} """
+@app.post("/login")
+async def login_user(user_credentials:schemas.UserLogin ,db: Session = Depends(get_db)):
+
+    user = db.query(tablesmodel.User).filter(tablesmodel.User.email==user_credentials.email).first()
+
+    if not user:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+    
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+    
+    access_token = oAuth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "Bearer"}
---
+++ app/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: str
+    password: str
+    phone_number: str
+    country: str
+    state: str
+
+class UserOut(BaseModel):
+    email: EmailStr
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True 
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str     
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None     
